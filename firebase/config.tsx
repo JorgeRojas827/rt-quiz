8e26092f915069ae3104b4cbe67b7128338d7c20
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2021-05-21 22:42:39 -0500

    Adding a little Firebase tech

---
+++ firebase/config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD3BRHo_Lz89totrhaIzlENqcLoLSvZlas",
+    authDomain: "rt-quiz-21ed3.firebaseapp.com",
+    projectId: "rt-quiz-21ed3",
+    storageBucket: "rt-quiz-21ed3.appspot.com",
+    messagingSenderId: "75645528424",
+    appId: "1:75645528424:web:7ebeffcc95676a1fcb8edd"
+}
